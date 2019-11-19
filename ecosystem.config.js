dfdcdef81696c7835c68f3b48273f77fa2730acb
xiaodong.li <xiaodong.li@asiainnovations.com>
2019-11-19 11:36:02 +0800

    add deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,5 +14,20 @@ module.exports = {
     env: {
       PORT: 9209
     }
-  }]
+  }],
+  deploy: {
+    production: {
+      'user': 'root',
+      'host': '47.104.216.30',
+      'ref': 'origin/master',
+      'repo': 'git@github.com:NextZeus/pm2-prometheus-exporter.git',
+      'path': '/data/work/pm2-prometheus-exporter',
+      'pre-setup': "yum install git -y;",
+      'post-setup': "ls -la",
+      'pre-deploy-local': "pwd; echo 'this is a local command' ",
+      'pre-deploy': 'pwd;ls -la;',
+      'post-deploy': 'git pull origin master;git log -n 2;',
+      'test': 'pwd;'
+    }
+  }
 };
